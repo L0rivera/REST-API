71e2873ed589ca18619346a7d2cbfae499a4bc8e
Luis098rivera <luis.rivera040606@gmail.com>
2024-07-25 20:48:35 -0500

    Cookie secure

--- REST-API/lib/CreateCookie.js
+++ REST-API/lib/CreateCookie.js
@@ -1,22 +1,26 @@
-import jsonwebtoken from "jsonwebtoken";
-import dotenv from "dotenv";
+// createcookie.js
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 function CreateCookie(res, username, email) {
   const token = jsonwebtoken.sign(
-    { username: username, email: email },
+    { username, email },
     process.env.JWT_SECRET,
     { expiresIn: process.env.JWT_EXPIRATION }
   );
 
-  const cookieOption = {
+  const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
-    path: "/",
+    path: '/',
+    httpOnly: true, // Agrega esta opción para mayor seguridad
+    secure: process.env.NODE_ENV === 'production' // Configura secure solo en producción
   };
-  res.cookie("jwt", token, cookieOption);
+
+  res.cookie('jwt', token, cookieOptions);
 }
 
 export default CreateCookie;
